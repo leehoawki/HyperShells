1bcf37294f62e379de03c25537542f59ff2f3266
leehoawki@gmail.com <leehoawki@gmail.com>
2018-12-21 13:23:42 +0800

    update

--- roll.py
+++ roll.py
@@ -5,8 +5,8 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--fr", help="default 0, included", type=int, default=0, action="store")
-    parser.add_argument("-t", "--to", help="default 100, not included", type=int, default=100, action="store")
+    parser.add_argument("-f", "--fr", help="default 1, included", type=int, default=1, action="store")
+    parser.add_argument("-t", "--to", help="default 100, included", type=int, default=100, action="store")
     parser.add_argument("-c", "--count", help="default 1", type=int, default=1, action="store")
     args = parser.parse_args()
     for i in range(0, args.count):
